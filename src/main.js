08db1df5739d76ecb677e32036a5c2af4d93cc3e
Eunjae Lee <eunjae.lee@algolia.com>
2019-12-12 14:11:23 +0100

    .

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { scrapDailyStat } from "./tasks/scrapDailyStat";
 
 export async function run() {
   const now = spacetime.now().goto("Europe/Paris");
+  console.log({ now, hour: now.hour() });
 
   if (now.hour() === 0) {
     await scrapDailyStat();
